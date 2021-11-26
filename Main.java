708337d7be9cc94674df5a1f8479c4a2fe2a46e6
Aditya Sahatonde <adityasahatonde555@gmail.com>
2021-11-26 20:42:17 +0530

    initial message

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String args[]){
+
+
+        int a=10;
+        int b=45;
+
+        System.out.println(a+b);
+    }
+}
